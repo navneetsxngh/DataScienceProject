3a91323fd6fb4bac48658ff5fceed5b61b445a6d
navneetsxngh <singhnavneet2587@gmail.com>
2026-05-12 15:29:37 +0530

    Data Validation Pipeline Completed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.datascience import logger
 from src.datascience.pipeline.dataingestionpipeline import DataIngestionTrainingPipeline
+from src.datascience.pipeline.datavalidationpipeline import DataValidationTrainingPipeline
 
 logger.info("Welcome to our Custom Logging Data Science")
 
@@ -9,6 +10,17 @@ try:
     logger.info(f">>>>>>>>Stage : {STAGE_NAME} Started <<<<<<<<<")
     obj = DataIngestionTrainingPipeline()
     obj.initiate_data_ingestion()
+    logger.info(f">>>>>>>> Stage : {STAGE_NAME} Completed <<<<<<<<")
+except Exception as e:
+    logger.error(e)
+    raise e
+
+STAGE_NAME="Data Validation Stage"
+
+try:
+    logger.info(f">>>>>>>>Stage : {STAGE_NAME} Started <<<<<<<<<")
+    obj = DataValidationTrainingPipeline()
+    obj.initiate_data_validation()
     logger.info(f">>>>>>>> Stage : {STAGE_NAME} Completed <<<<<<<<")
 except Exception as e:
     logger.error(e)
---
+++ src/datascience/pipeline/datavalidationpipeline.py
@@ -0,0 +1,19 @@
+from src.datascience.config.configuration import ConfigurationManager
+from src.datascience.components.datavalidation import DataValidation
+from src.datascience import logger
+
+STAGE_NAME="Data Validation Stage"
+
+class DataValidationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def initiate_data_validation(self):
+        try:
+            config=ConfigurationManager()
+            data_validation_config=config.get_data_validation_config()
+            data_validation=DataValidation(config=data_validation_config)
+            data_validation.validate_all_columns()
+        except Exception as e:
+            logger.error(e)
+            raise e
